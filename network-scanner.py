e55d43e3cb97620f560b39cb3b7cbb5a469bbf68
benterem <benterem@gmail.com>
2021-09-08 17:41:45 +0300

    ARP request

--- network-scanner.py
+++ network-scanner.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-import scapy.all as scapy
+from scapy import all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    print(arp_request.summary())
+    scapy.ls(scapy.ARP())
 
-
-scan("192.168.51.1")
-
+scan("192.1.1.1/24")
