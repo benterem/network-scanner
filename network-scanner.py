60534d6c4f4d4b40c60982cf46e2007d2457c4db
benterem <benterem@gmail.com>
2021-09-09 18:39:59 +0300

    enter ip with command line args

--- network-scanner.py
+++ network-scanner.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 
 from scapy import all as scapy
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target_ip", dest="target_ip", help="Target IP for which to find its MAC address")
+    parser.add_option("-m", "--mask", dest="mask", help="Network mask for IP")
+    (options, arguments) = parser.parse_args()
+    
+    if not options.target_ip:
+        parser.error("[-] Please specify an IP address, use --help for more info")
+    if not options.mask:
+        return options.target_ip
+    
+    return options.target_ip + "/" + options.mask
 
 
 def scan(ip):
@@ -14,7 +28,6 @@ def scan(ip):
     for target in answered_list:
         target_clients.append({"mac": target[1].hwsrc, "ip": target[1].psrc})
 
-    print(target_clients)
     return target_clients
 
 def print_results(targets_list):
@@ -26,6 +39,6 @@ def print_results(targets_list):
         print(f"{target['ip']}\t\t{target['mac']}")
         print("\n") 
 
-
-targets_list = scan("192.168.1.69/24")
+target_ip = get_arguments()
+targets_list = scan(target_ip)
 print_results(targets_list)
