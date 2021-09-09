f31393d9614f5bb147028b2aa9398d795e2051a7
benterem <benterem@gmail.com>
2021-09-09 18:01:55 +0300

    format

--- network-scanner.py
+++ network-scanner.py
@@ -7,13 +7,14 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_req_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_req_broadcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_req_broadcast, timeout=1, verbose=False)[0]
     
-    print("\n")
+    print("_________________________________________\n")
+    print("IP\t\t\tMAC ADDRESS")
+    print("_________________________________________\n")
 
     for element in answered_list:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
+        print(f"{element[1].psrc}\t\t{element[1].hwsrc}")
         print("\n") 
 
 
